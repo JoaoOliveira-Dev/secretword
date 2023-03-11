21e20cefb40b974f872c3a8ed35e2c5787410b4f
JoaoOliveira-Dev <joaooli17@hotmail.com>
2023-03-10 21:30:32 -0300

    Estilização do component de game

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,9 +3,30 @@ import "./Game.css";
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game  </h1>
-      <button onClick={verifyLetter}>Finalizar jogo!</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra</h1>
+      <h3 className="tip">
+        Dicas sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="lettersContainer">
+        <p>Tente adivinhar a letra:</p>
+        <form>
+          <input type="text" className="letter" maxLength= "1" required/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContaner">
+        <p>Letras erradas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
   )
 }
